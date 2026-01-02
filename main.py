f4340c89d4128f6fd8cc23712378f59e5b4e0430
Xushnudd <boshhacker1@gmail.com>
2026-01-02 22:03:22 +0500

    Done

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 from tinydb import TinyDB, Query
 
 app = Flask(__name__)
+CORS(app)
 
 db = TinyDB("db.json")
 user = Query()
